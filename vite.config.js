dfc5cb5a92d8e7f2f82190266c75c2edfa1fe44c
Ruben Cantero <rubenc123369@gmail.com>
2025-05-26 18:44:41 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/centro-deportivo/', // Esta línea es crucial para GitHub Pages
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist', // Especifica la carpeta de construcción
+    emptyOutDir: true // Limpia la carpeta antes de cada build
   }
 })
